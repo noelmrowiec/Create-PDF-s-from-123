a76345921645ff9a6755c6d12a91cf0cad4899bf
noelmrowiec <noelmrowiec@gmail.com>
2023-03-16 21:20:24 +0100

    tested get_items_to_print_to_1750

--- test_get_data_from sheet.py
+++ test_get_data_from sheet.py
@@ -2,9 +2,12 @@ import sys
 import pandas as pd    
 
 def get_items_to_print_to_1750(inventory):
-    items_to_print = inventory['PRINT DD-1750']
+    # returns items from inventory that have the 'x' or 'X' in the column to print the 1750
+    items_to_print = inventory[inventory['PRINT DD-1750'] == 'x']
+    print(items_to_print)
+
 
 inventory = pd.read_excel('test 123.xlsx')
-
+get_items_to_print_to_1750(inventory)
 
 
