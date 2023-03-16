f24ebe7fe7094d478d08efe7f17a2ae79ac973ba
noelmrowiec <noelmrowiec@gmail.com>
2023-03-16 21:12:39 +0100

    adding test for get_items_to_print_to_1750

--- test_get_data_from sheet.py
+++ test_get_data_from sheet.py
@@ -1,4 +1,10 @@
 import sys
 import pandas as pd    
 
+def get_items_to_print_to_1750(inventory):
+    items_to_print = inventory['PRINT DD-1750']
+
+inventory = pd.read_excel('test 123.xlsx')
+
+
 
