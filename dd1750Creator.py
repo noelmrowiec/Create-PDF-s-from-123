5a5328225478d6aa8472057afd4c5d5b2fb5e9bd
noelmrowiec <noelmrowiec@gmail.com>
2023-03-17 10:57:55 +0100

    updated names

--- dd1750Creator.py
+++ dd1750Creator.py
@@ -53,25 +53,26 @@ def get_items_to_print_to_1750(inventory):
     items_to_print = inventory[inventory['PRINT DD-1750'] == 'x']
     return items_to_print
 
+''' returns: a DataFrame object with items combined 
+'''
 def combine_same_items(inventory):
     #find all common names
     #combine serials 
     #either ouput or save
     #define how to aggregate various fields
-    agg_functions = {'SERIAL': 'sum'}
 
     #create new DataFrame by combining rows with same id values
     #https://www.statology.org/pandas-combine-rows-with-same-column-value/
     #source https://stackoverflow.com/questions/33279940/how-to-combine-multiple-rows-of-strings-into-one-using-pandas
     #https://pandas.pydata.org/
     #df_new = inventory.groupby('COMMON NAME')
-    print(inventory.info())
+    #print(inventory.info())
     # below groups items with the same common name the aggregates the serial number into one comma separted string with all of the serial number 
-    df_new = inventory.groupby('COMMON NAME').aggregate({
+    df_combined = inventory.groupby('COMMON NAME').aggregate({
         'SERIAL': lambda x: ', '.join([str(i) for i in x])
     })
     
-    print(df_new)
+    return df_combined
         
 # prompt user for input excel sheet file name
 inventory = get_inventory()
