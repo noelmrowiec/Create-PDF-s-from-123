06f473aa43c5ffa3c7ee1549213a5e7dd5c027e7
noelmrowiec <noelmrowiec@gmail.com>
2023-03-17 11:12:52 +0100

    testing get_items_to_print_to_1750 ofr correct data

--- test_area.py
+++ test_area.py
@@ -23,6 +23,12 @@ def combine_same_items(inventory):
     
 
 inventory = pd.read_excel('test 123.xlsx')
-something(inventory)
+def get_items_to_print_to_1750(inventory):
+    # returns items from inventory that have the 'x' in the column to print the 1750
+    # todo right now, must be lower case 'x'. check for both
+    items_to_print = inventory[inventory['PRINT DD-1750'] == 'x']
+    return items_to_print
+print(get_items_to_print_to_1750(inventory)
+
 
 
