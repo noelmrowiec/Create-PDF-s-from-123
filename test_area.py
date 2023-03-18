a5aa8c47a62466d5f57efd84fd67991f7fe4c4e0
noelmrowiec <noelmrowiec@gmail.com>
2023-03-18 12:54:32 +0100

    found method for spliting lines

--- test_area.py
+++ test_area.py
@@ -1,6 +1,15 @@
+from operator import index
 import sys
 import pandas as pd    
 
-
-
-
+line = ['aaaaaaaaaaaa', 'ffffffffffffff', 'ggggggggggggggg','hhh', 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj','kkkkkkkkk' ]
+n = 12
+newList=  []
+for element in line:
+    if len(element)> 12:
+        """Produce `n`-character chunks from `s`."""
+        for start in range(0, len(element), n):
+            newList.append(element[start:start+n])
+    else:
+        newList.append(element)
+print(newList)
