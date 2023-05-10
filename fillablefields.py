af177143c6e922d753a1783d6c3b1a879f06f6aa
noelmrowiec <noelmrowiec@gmail.com>
2023-05-10 20:57:49 +0200

    fixed MAX_ITEMS error

--- fillablefields.py
+++ fillablefields.py
@@ -33,7 +33,7 @@ class FillableFields(object):
     line_num: line number, for 
     '''
     def add_box_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'box_{line_num}'] = text
             return True
         else:
@@ -45,7 +45,7 @@ class FillableFields(object):
     returns: True is text for the contents field is added to the contents list, otherwise false if the length of the contents list is greater than MAX_ITEMS
     '''
     def add_contents_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'contents_{line_num}'] = text
             return True
         else:
@@ -55,7 +55,7 @@ class FillableFields(object):
     returns: True is text for the init field is added to the init list, otherwise false if the length of the init list is greater than MAX_ITEMS
     '''
     def add_init_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'init_{line_num}'] = text
             return True
         else:
@@ -65,7 +65,7 @@ class FillableFields(object):
     returns: True is text for the spares field is added to the spares list, otherwise false if the length of the spares list is greater than MAX_ITEMS
     '''
     def add_spares_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'spares_{line_num}'] = text
             return True
         else:
@@ -75,7 +75,7 @@ class FillableFields(object):
     returns: True is text for the total field is added to the total list, otherwise false if the length of the total list is greater than MAX_ITEMS
     '''
     def add_total_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'total_{line_num}'] = text
             return True
         else:
@@ -85,7 +85,7 @@ class FillableFields(object):
     returns: True is text for the uint field is added to the uint list, otherwise false if the length of the uint list is greater than MAX_ITEMS
     '''
     def add_uint_field(self, text, line_num):
-        if(line_num < self.MAX_ITEMS):
+        if(line_num <= self.MAX_ITEMS):
             self.ff_dict[f'uint_{line_num}'] = text
             return True
         else:
